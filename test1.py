9e52a516abf646215018ca285342288c9f1f4290
small_teather <gym625.ysmur@yandexlyceum.ru>
2020-03-20 16:10:37 +0300

    Задача 1. Запрос Записи на стене

---
+++ test1.py
@@ -0,0 +1,24 @@
+import vk_api
+import datetime
+
+
+def main():
+    login, password = LOGIN, PASSWORD # здесь настоящие данные пользователя
+    vk_session = vk_api.VkApi(login, password)
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+    vk = vk_session.get_api()
+    # Используем метод wall.get
+    response = vk.wall.get(count=5, offset=0)
+    if response['items']:
+        for i in response['items']:
+            value = datetime.datetime.fromtimestamp(i['date'])
+            print(value.strftime('%Y-%m-%d'))
+            print(i['text'])
+
+
+if __name__ == '__main__':
+    main()
